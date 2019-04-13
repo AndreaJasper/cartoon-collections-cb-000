690ff9d0888702cdf9d29701d13f47046098963a
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-13 23:07:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(arr)# code an argument here
   array.any?{|word| word.length < 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
+  arr.include
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
